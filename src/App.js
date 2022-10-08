e2ac25fa18ac8bdd9e11451ddee1bdd27a7faacb
Xuyang Liu <sophieliu625@gmail.com>
2022-10-08 00:39:30 -0400

    update to ui-react-v1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1';
 function App() {
   return (
     <div className="App">
